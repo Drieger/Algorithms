af944389021819d38061c4d925f5c0e4823ed806
Daniel Beckert <drbeckert@gmail.com>
2017-01-14 15:53:24 -0200

    Change folder structure

---
+++ javascript/sort/insertion_sort.js
@@ -0,0 +1,22 @@
+/*
+ * Basic implementation of sorting algorithms.
+ * Based on the book: Introduction to Algorithms - (https://mitpress.mit.edu/books/introduction-algorithms)
+ *
+ * All implementations here take as input a single unordered array of integers
+ * No input validation is performed
+ */
+
+
+function insertionSort(arr) {
+  if ( arr.length <= 1 ) return arr;
+  for (var j=1; j<arr.length; j++) {
+    var key = arr[j],
+        idx = j - 1;
+    while (idx>=0 && arr[idx]>key) {
+      arr[idx + 1] = arr[idx];
+      idx--;
+    }
+    arr[idx + 1] = key;
+  }
+  return arr;
+}
